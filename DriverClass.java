7bdfd0e2d4d6d593557dad6f15144bb876df2183
Krishnaaron <krishsri520@gmail.com>
2024-05-21 15:30:57 +0530

    DriverClass changes complete

--- DriverClass.java
+++ DriverClass.java
@@ -1,44 +1,59 @@
-package com.streams;
+/*
+ * The childthread class is create to 
+ */
+package com.multithreading;
+public class ChildThread extends Thread {
 
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+	public void run() {
+		try {
+			Thread.sleep(1000);
+			Thread.interrupted();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch bloc
+			e.printStackTrace();
+		}
+		for (int i = 0; i < 5; i++) {
 
-public class DriverClass{
+			System.out.println("child 1: " + i);
+		}
+	}
 
-	public static void main(String[] args) {
-		
-		//Stream Iterate returns void
-		Stream.iterate(1,next->next+1).filter(list->list%5==0).limit(5)
-											.forEach(value -> System.out.print(value+"\t"));
-		//usage of map and collect method
-		@SuppressWarnings("unused")
-		List<Integer> listStream=Stream.iterate(1,next->next+1).filter(list->list%5==0).limit(5).map(value->value*2)
-				.collect(Collectors.toList());
-		System.out.println("\n"+listStream);
-		
-		//with Strings - usage of filter method
-		Collection<String> collection = Arrays.asList("Model", "Race", "Race","Reflection","Model","Spain","Football");
-		List<String> streamOfCollection = collection.stream()
-											.filter(s->s.startsWith("R"))
-											.collect(Collectors.toList());
-		System.out.println(streamOfCollection);
-		
-		//usage of sorted method
-		List<String> streamStringSorted = collection.stream().sorted().collect(Collectors.toList());
-		System.out.println(streamStringSorted);
-		
-		//usage of reduce method
-		Integer [] numbers = new Integer[] {4,2,6,1,7,3,3,9};
-		int odd = Arrays.stream(numbers).filter(x->x%2!=0).distinct().map(x->x*2).re//reduce(0,(ans,i)-> ans+i);
-		System.out.println(odd);
-		//int sum = Arrays.stream(numbers).a
-		//usage of set
-		Set<String> streamStringSet = collection.stream().collect(Collectors.toSet());
-		System.out.println(streamStringSet);
-		
+}
+
+class Child2Thread extends Thread {
+
+	public void run() {
+
+		for (int i = 0; i < 5; i++) {
+
+			System.out.println("child 2 : " + i);
+
+		}
+
 	}
 }
+
+class Child3Thread extends Thread {
+
+	public void run() {
+		for (int i = 0; i < 5; i++) {
+
+			System.out.println("child3 : " + i);
+		}
+
+	}
+}
+
+class Task4 implements Runnable {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 5; i++) {
+
+			System.out.println("Runnable : " + i);
+		}
+
+	}
+
+}	
